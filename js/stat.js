0ad4a1a4b8f58c47567149ca083e0d01d92d54ef
Andrey <eckos997@gmail.com>
2018-11-22 18:26:12 +0500

    Вынес константы

--- js/stat.js
+++ js/stat.js
@@ -2,17 +2,36 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var CLOUD_COLOR = '#ffffff';
+var CLOUD_BGCOLOR = 'rgba(0, 0, 0, 0.7)';
 var TEXT_FONT = '16px PT Mono';
 var TEXT_COLOR = '#000000';
 var CHART_HEIGHT = 150;
+var ChartPosition = {
+  x: 140,
+  y: 245
+};
+var TEXT_LINE1 = 'Ура вы победили!';
+var TEXT_LINE2 = 'Список результатов:';
+var TextLinePosition1 = {
+  x: 120,
+  y: 40
+};
+var TextLinePosition2 = {
+  x: 120,
+  y: 60
+};
 var COLUMN_GAP = 50;
 var COLUMN_WIDTH = 40;
+var SUP_RANGE = 100;
+var NAME_GAP = 20;
+var SCORE_GAP = 10;
 
 var renderCloud = function (ctx, x, y) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillStyle = CLOUD_BGCOLOR;
   ctx.fillRect(x + 10, y + 10, CLOUD_WIDTH, CLOUD_HEIGHT);
 
-  ctx.fillStyle = '#ffffff';
+  ctx.fillStyle = CLOUD_COLOR;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
@@ -39,27 +58,25 @@ var getColumnHeightArray = function (times) {
 var renderChart = function (ctx, names, times) {
   var columnHeights = getColumnHeightArray(times);
   var fillColor;
-  var x = 140;
-  var y = 245;
 
   for (var i = 0; i < times.length; i++) {
     if (names[i] === 'Вы') {
       fillColor = 'red';
     } else {
-      var randomSaturate = Math.floor(Math.random() * (100 - 1)) + 1;
+      var randomSaturate = Math.floor(Math.random() * (SUP_RANGE - 1)) + 1;
       fillColor = 'hsl(240, ' + randomSaturate + '%, 50%';
     }
     ctx.fillStyle = fillColor;
-    ctx.fillRect(x, y, COLUMN_WIDTH, -columnHeights[i]);
-    renderText(names[i], x, y + 20, ctx);
-    renderText(Math.trunc(times[i]), x, y - columnHeights[i] - 10, ctx);
-    x += COLUMN_GAP + COLUMN_WIDTH;
+    ctx.fillRect(ChartPosition.x, ChartPosition.y, COLUMN_WIDTH, -columnHeights[i]);
+    renderText(names[i], ChartPosition.x, ChartPosition.y + NAME_GAP, ctx);
+    renderText(Math.trunc(times[i]), ChartPosition.x, ChartPosition.y - columnHeights[i] - SCORE_GAP, ctx);
+    ChartPosition.x += COLUMN_GAP + COLUMN_WIDTH;
   }
 };
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 100, 10);
-  renderText('Ура вы победили!', 120, 40, ctx);
-  renderText('Список результатов:', 120, 60, ctx);
+  renderText(TEXT_LINE1, TextLinePosition1.x, TextLinePosition1.y, ctx);
+  renderText(TEXT_LINE2, TextLinePosition2.x, TextLinePosition2.y, ctx);
   renderChart(ctx, names, times);
 };
